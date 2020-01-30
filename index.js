bacfc5514d650b8d6c7b8063b1f650d3ab3faa57
Henry-Bernardin <henry.bernardin@gmail.com>
2020-01-30 15:18:55 -0500

    input created, along with search logic

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd3f44806', 
+            s: searchTerm
+        }
+    });
 
-}
+    console.log(response.data);
+}; 
+
+const input = document.querySelector('input'); 
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+}); 
+
