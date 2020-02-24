bcb59fe38334464dd9cf62ae1d003cbc1f0b4de0
Henry-Bernardin <henry.bernardin@gmail.com>
2020-02-23 23:56:39 -0500

    Parsing numbers of awards

--- index.js
+++ index.js
@@ -72,8 +72,17 @@ const movieTemplate = movieDetail => {
     const metascore = parseInt(movieDetail.Metascore);
     const imdbRating = parseFloat(movieDetail.imdbRating);
     const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word);
 
-    console.log(imdbRating, imdbVotes, metascore);
+        if (isNaN(value)) {
+            return prev;
+        } else {
+            return prev + value; 
+        }
+    }, 0);
+
+    console.log(imdbRating, imdbVotes, metascore, dollars);
 
     return `
     <article class="media">
