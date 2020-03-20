1bd132aa07046a8fa6308c35cf297b9f05475eba
Henry-Bernardin <henry.bernardin@gmail.com>
2020-03-19 23:24:16 -0400

    semi colon added

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const debounce = (func, delay = 1000) => {
     return (...args) => {
         if (timeoutId){
             clearTimeout(timeoutId) 
-        }
+        };
         timeoutId = setTimeout(() => {
             func.apply(null, args);
         }, delay)
